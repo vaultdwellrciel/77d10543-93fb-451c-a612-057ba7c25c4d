33e61f5ea7e0a45a44767d8ee6ff7d9473b90829
Marc Mailhot <marc.mailhot@kik.com>
2016-04-04 21:49:49 -0400

    Fix setup.cfg

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='kik',
-    version='1.0.0',
+    version='1.0.1',
     packages=['kik', 'kik.messages'],
     package_dir={
         'kik': 'kik',
